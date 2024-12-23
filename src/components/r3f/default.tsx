a0cfdf0757bd5467da24d1785eba444bffa1d549
tatsuhiko sakai <seahorse2000@gmail.com>
2024-12-23 23:57:53 +0900

    add - three

--- src/components/r3f/default.tsx
+++ src/components/r3f/default.tsx
@@ -1,4 +1,4 @@
-import { CameraControls } from '@react-three/drei';
+import { CameraControls, Grid } from '@react-three/drei';
 
 export default function DefaultReact() {
   return (
@@ -6,10 +6,11 @@ export default function DefaultReact() {
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
       <CameraControls />
-      <mesh receiveShadow castShadow>
+      <mesh receiveShadow castShadow position={[0, 1, 0]}>
         <boxGeometry />
         <meshStandardMaterial color="red" />
       </mesh>
+      <Grid position={[0, 0, 0]}  infiniteGrid={true} fadeDistance={20} />
     </>
   );
 }
--- src/components/threejs.tsx
+++ src/components/threejs.tsx
@@ -1,25 +1,10 @@
 import { Canvas } from '@react-three/fiber';
 import * as THREE from 'three';
 import DefaultThree from './r3f/default';
+import TestScene from './r3f/testScene';
 
 export default function Threejs() {
   return (
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [2, 2, -2],
-      }}
-      gl={{
-        toneMapping: THREE.ACESFilmicToneMapping,
-        toneMappingExposure: 1.0,
-      }}
-      style={{
-        background: '#000000'
-      }}
-    >
-      <DefaultThree />
-    </Canvas>
+    <TestScene />
   );
 }
