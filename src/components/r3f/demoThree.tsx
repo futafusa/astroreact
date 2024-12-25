f7efaadcc8e6a820df1c8756722eb2ef0264a08a
tatsuhiko sakai <seahorse2000@gmail.com>
2024-12-25 21:29:50 +0900

    add - demoシーン作成

---
+++ src/components/r3f/demoThree.tsx
@@ -0,0 +1,67 @@
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+import { ScrollControls, useScroll } from '@react-three/drei';
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+
+function Plane() {
+  const refPlane = useRef<THREE.Mesh>(null);
+  const data = useScroll();
+
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      uTime: { value: 0 },
+    },
+    vertexShader: `
+      void main() {
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      }
+    `,
+    wireframe: true,
+    side: THREE.DoubleSide,
+  });
+
+  useFrame((state, delta) => {
+    const amountScroll = data.range(0, 1);
+
+    if (refPlane.current) {
+      material.uniforms.uTime.value += delta;
+      refPlane.current.rotation.x = amountScroll * Math.PI * 2;
+    }
+  });
+
+  return (
+    <mesh ref={refPlane} material={material} rotation={[0, Math.PI, 0]}>
+      <planeGeometry args={[1, 1, 32, 32]} />
+    </mesh>
+  );
+}
+
+export default function DemoThree() {
+  return (
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [0, 0, -1.5],
+      }}
+      gl={{
+        toneMapping: THREE.ACESFilmicToneMapping,
+        toneMappingExposure: 1.0,
+      }}
+      style={{
+        background: '#000000'
+      }}
+    >
+      <ScrollControls pages={3} damping={0.4}>
+        <Plane />
+      </ScrollControls>
+    </Canvas>
+  );
+}
--- src/components/threejs.tsx
+++ src/components/threejs.tsx
@@ -2,9 +2,13 @@ import { Canvas } from '@react-three/fiber';
 import * as THREE from 'three';
 import DefaultThree from './r3f/default';
 import TestScene from './r3f/testScene';
+import DemoThree from './r3f/demoThree';
 
 export default function Threejs() {
   return (
-    <TestScene />
+    <>
+      {/* <TestScene /> */}
+      <DemoThree />
+    </>
   );
 }
